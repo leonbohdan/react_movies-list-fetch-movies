cf8bb583e83cf29c227bfbd4ba092f3a9f6ff878
Bohdan Leonets <leonetsbogdan@gmail.com>
2020-09-15 15:03:16 +0300

    Updates

---
+++ precache-manifest.be913e7aaec6705f0b17abb1cd538f84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea2d3d7d03d80eafa9f02dc53f44357e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bef337ee2440f09ca59f",
+    "url": "./static/css/2.67a0cd86.chunk.css"
+  },
+  {
+    "revision": "66a3444a39299971a4f8",
+    "url": "./static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "bef337ee2440f09ca59f",
+    "url": "./static/js/2.11e02830.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.11e02830.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66a3444a39299971a4f8",
+    "url": "./static/js/main.216f1005.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
